dcdaa2942f67d9c5cf6d38dd17f8df24ca1379cb
hat082 <2798638863@qq.com>
2024-09-20 22:18:20 +0100

    Inital commit: Created a simple flake template for cpp

---
+++ README.md
@@ -0,0 +1,8 @@
+# CPP DEV FLAKE
+A basic template for cpp development modified from this repo: https://github.com/tfc/cpp-nix-2023/tree/part1
+
+Usage:
+```shell
+git flake init -t https://github.com/hat082/cpp-template.git
+direnv allow .
+```
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "C++ Dev with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # devShells.default describes the default shell with C++, cmake, boost,
+        # and catch2
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # C++ Compiler is already part of stdenv
+              boost
+              catch2
+              cmake
+            ];
+          };
+
+          # a shell that works on macos 
+          clang = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            packages = with pkgs; [
+              boost
+              catch2
+              cmake
+            ];
+          };
+        };
+      };
+    };
+}
